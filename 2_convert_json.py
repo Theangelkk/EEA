68946fa89352cbf867a9ac057d4723a9d9f499a9
Theangelkk <47244087+Theangelkk@users.noreply.github.com>
2023-11-06 09:09:05 +0100

    update 2_convert_json.py

--- 2_convert_json.py
+++ 2_convert_json.py
@@ -1,4 +1,6 @@
-# Librerie necessarie
+# conda activate EEA
+
+# Libreries
 import os
 import sys
 
@@ -11,10 +13,26 @@ import airbase
 import json
 import gc
 
+import warnings
+import asyncio
+
+import platform
+if platform.system()=='Windows':
+    asyncio.set_event_loop_policy(asyncio.WindowsSelectorEventLoopPolicy())
+
+warnings.filterwarnings("ignore")
+
 def joinpath(rootdir, targetdir):
     return os.path.join(os.sep, rootdir + os.sep, targetdir)
 
-# Creazione del dizionario delle regioni italiane
+# Path of EEA_data
+path_main_dir_EEA_data = os.environ['EEA_data']
+
+if path_main_dir_EEA_data == "":
+    print("Error: set the environmental variables of EEA data")
+    exit(-1)
+
+# Function related on creation of empty Italy dictionary
 def empty_dict_italy_region():
 
     list_italy_region = [   "Abruzzo", "Basilicata", "Calabria", "Campania", "Emilia-Romagna", \
@@ -29,17 +47,17 @@ def empty_dict_italy_region():
         dict_current_region["N_stations"] = 0
         dict_current_region["stations"] = {}
 
-        # Numero totale delle misurazioni
+        # Total number of measures
         dict_current_region["N_tot_measures"] = 0
 
-        # Numero totale delle misurazioni valide
+        # Total number of valid measures
         dict_current_region["N_tot_valid_measures"] = 0
 
-        # Numero totale dei valori mancanti
+        # Total number of missing values
         dict_current_region["N_tot_missing_values"] = 0
 
-        # Lista ordinata stazioni con minori tratti di missing values
-        # e di minor lunghezza media
+        # Sorted list of the stations with minumin number of
+        # missing values in the maximum lenght of missing data tract
         dict_current_region["sorted_station_MDTs_min"] = []
 
         dict_italy_region[region] = dict_current_region
@@ -54,36 +72,36 @@ def fill_dict_station(
 
     dict_info_station = {}
 
-    # Numero dei tratti di valori mancanti
+    # Total number of Missing Data Tracts
     dict_info_station["Number_of_MDTs"] = Number_of_MDTs
 
-    # Lunghezza massima di un tratto di valori mancanti
+    # Maximum lenght of Missing Data Tracts
     dict_info_station["Max_len_of_MDTs"] = Max_len_of_MDTs
 
-    # Lunghezza minima di un tratto di valori mancanti
+    # Minimum lenght of Missing Data Tracts
     dict_info_station["Min_len_of_MDTs"] = Min_len_of_MDTs
 
-    # Lunghezza media di un tratto di valori mancanti
+    # Average lenght of Missing Data Tracts
     dict_info_station["Avg_len_of_MDTs"] = Avg_len_of_MDTs
 
-    # Numero totale delle misurazioni
+    # Total number of measures
     dict_info_station["N_tot_measures"] = N_tot_measures
 
-    # Numero totale delle misurazioni valide
+    # Total number of valid measures
     dict_info_station["N_tot_valid_measures"] = N_tot_valid_measures
 
-    # Numero totale dei valori mancanti
+    # Total number of missing values
     dict_info_station["N_tot_missing_values"] = N_tot_missing_values
 
     return dict_info_station
 
-# Per accedere al servizio airbase, è necessario creare prima un aribase client
+# Client of Airbase
 client = airbase.AirbaseClient()
 
 list_air_pollutant = ["CO", "NO2", "O3", "PM2.5", "PM10", "SO2"]
 list_countries = client.all_countries
 
-parser = argparse.ArgumentParser(description='Metadata download of EEA')
+parser = argparse.ArgumentParser(description='Convert text information info Json file of EEA stations')
 parser.add_argument('-a', '--air_pollutant', help='Air Pollutant CO - NO2 - O3 - PM2.5 - PM10 - SO2', choices=list_air_pollutant, required=True)
 parser.add_argument('-c', '--country', help='Country to observe', choices=list_countries, required=True)
 parser.add_argument('-freq_mode', '--freq_mode', help='Frequency mode (hour, day)', type=str, required=True)
@@ -97,39 +115,46 @@ freq_mode = args["freq_mode"]
 start_year = int(args["start_year"])
 end_year = int(args["end_year"])
 
-# Path relativo ai metainfo files
-path_metainfo_file = joinpath(os.getcwd(), "new_metainfo")
-path_metainfo_file = joinpath(path_metainfo_file, air_poll_selected + "_" + country + ".csv")
+if start_year > end_year:
+   print("End_year must be greater then start_year!")
+   exit(-1)
 
-# Path relativo ai dati memorizzati in formato CSV
-path_dir_csv = joinpath(os.getcwd(), "csv_air_pol")
-path_dir_air_pol_csv = joinpath(path_dir_csv, air_poll_selected)
-path_dir_air_pol_csv = joinpath(path_dir_air_pol_csv, freq_mode)
+# Path of metainfo files
+path_metainfo_file = joinpath(path_main_dir_EEA_data, "New_metainfo")
+path_metainfo_file = joinpath(path_metainfo_file, air_poll_selected + "_" + country + "metainfo.csv")
 
-# Ricerca del file CSV dei dati
-list_files_dir_air_pol_csv = os.listdir(path_dir_air_pol_csv)
+# Path of CSV EEA stations data
+path_dir_csv = joinpath(path_main_dir_EEA_data, "EEA_data")
+path_dir_csv = joinpath(path_dir_csv, country)
+path_dir_csv = joinpath(path_dir_csv, air_poll_selected)
+path_dir_csv = joinpath(path_dir_csv, freq_mode)
+
+# Find CSV data file
+list_files_dir_air_pol_csv = os.listdir(path_dir_csv)
 path_file_air_pol_data_csv = None
 
 for file in list_files_dir_air_pol_csv:
     if file.endswith('.csv'):
-        path_file_air_pol_data_csv = joinpath(path_dir_air_pol_csv, file)
+        path_file_air_pol_data_csv = joinpath(path_dir_csv, file)
         break
 
-# Lettura del file di dati dell'inquinante richiesto
+# Read CSV file of the air pollutant specified
+	
+if air_poll_selected == "NO2":
+  low_memory_flag = True
+else:
+  low_memory_flag = False
+
 df_air_pol_data = pd.read_table(    path_file_air_pol_data_csv, delimiter=',', 
-                                    header=[0], index_col=0, low_memory=False
+                                    header=[0], index_col=0, low_memory=low_memory_flag
                                 )
 
-if end_year <= start_year:
-    print("Error: Start year must be greater then end year")
-    exit(-1)
-
-# Lettura del file di metainfo dell'inquinante della nazione definito
+# Read metainfo file about the air pollutant and country defined
 df_air_pol_metainfo = pd.read_table(path_metainfo_file, delimiter=',', index_col=0)
 
 current_year = start_year
 
-# Si analizzano il range temporale definito
+# For each year asked
 for idx_year in range(start_year,end_year):
 
     print("Current year: ", str(current_year))
@@ -141,8 +166,8 @@ for idx_year in range(start_year,end_year):
     else:
         delta = 24
     
-    # Lettura file di testo
-    with open(joinpath(path_dir_air_pol_csv, str(current_year) + ".txt")) as f:
+    # Read text file of current year analyzed
+    with open(joinpath(path_dir_csv, str(current_year) + ".txt")) as f:
         lines_current_year = f.readlines()
 
     if country == "IT":
@@ -151,114 +176,107 @@ for idx_year in range(start_year,end_year):
         print("Implement for the country specified")
         exit(-1)
 
-    # Andiamo a scorrere tutte le stazioni
+    # For all station of current year
     for cod_station in lines_current_year:
 
-        # Se la linea che stiamo osservando è effettivamente
-        # una stazione
+        # Check about the validity of cod_station
         if country in cod_station:
             
             cod_station = cod_station.replace("\n","")
 
             print("Current code station: ", cod_station)
 
-            # Andiamo a recuperare le info della stazione corrente all'interno
-            # del file di metainfo
+            # Query on metainfo due to cod_station
             df_current_station = df_air_pol_metainfo.filter(like=cod_station, axis=0)
 
             current_station_region = df_current_station["Regione"].values[0]
 
             dict_country_current_year[current_station_region]["N_stations"] += 1
         
-            # --------------------- Calcolo Missing values --------------------- 
-            # Andiamo a prendere tutte le rilevazioni di una specifica stazione
+            # --------------------- Computing Missing values --------------------- 
+
+            # Query on csv file due to cod_station
             df_current_station = df_air_pol_data.filter(like=cod_station, axis=0)
             
-            # Ordinamento del DataFrame secondo la data di acquisizione
+            # Sorting by DatetimeBegin
             df_current_station = df_current_station.sort_values(by='DatetimeBegin')
 
-            # Numero delle misurazioni totali
+            # Inizialization of counting variables of current station
             count_total_measures_current_station_current_year = 0
-
-            # Numero delle misurazioni valide
             count_valid_measures_current_station_current_year = 0
-
-            # Numeri dei missing values
             count_nan_measures_current_station_current_year = 0
-
-            # Lunghezza massima di un tratto di missing values
             len_max_missing_values_current_station_current_year = 0
-
-            # Lunghezza minima di un tratto di missing values
             len_min_missing_values_current_station_current_year = np.inf
-
-            # Lunghezza media di un tratto di missing values
             len_avg_missing_values_current_station_current_year = 0
-
-            # Numero di tratti di valori mancanti continui
             count_missing_data_tracts_current_station_current_year = 0
 
-            # Se siamo in un tratto di valori mancanti
+            # If it has been observed a new Missing Data Tract
             current_MDT = False
             current_len_MDT = 0
             
             first_day = True
 
-            for idx, row in df_current_station.iterrows():
+            # Retrive measures of current station about the current year considered
+            start_date_current_year = str(current_year) + "-01-01"
+            end_date_current_year = str(current_year) + "-12-31"
+
+            mask =  (df_current_station['DatetimeBegin'] >= start_date_current_year) & \
+                    (df_current_station['DatetimeBegin'] <= end_date_current_year)
+    
+            df_current_station_current_year = df_current_station.loc[mask]
+
+            for idx, row in df_current_station_current_year.iterrows():
                 
-                if str(current_year) in row["DatetimeBegin"]:
+                # It it is the first measure
+                if first_day:
+                    first_date_current_station = \
+                        datetime.strptime(row["DatetimeBegin"].replace(' +01:00', ''), '%Y-%m-%d %H:%M:%S')
 
-                    if first_day:
-                        first_date_current_station = \
-                                datetime.strptime(row["DatetimeBegin"].replace(' +01:00', ''), '%Y-%m-%d %H:%M:%S')
+                    diff_dates = first_date_current_station - start_date_current_year
+                    diff_dates = int(diff_dates.total_seconds() / (60*60*delta))
 
-                        diff_dates = first_date_current_station - start_date_current_year
-                        diff_dates = int(diff_dates.total_seconds() / (60*60*delta))
-
-                        if diff_dates > 0:
-                            count_missing_data_tracts_current_station_current_year += 1
-                            len_max_missing_values_current_station_current_year = diff_dates
-                            len_min_missing_values_current_station_current_year = diff_dates
-                            len_avg_missing_values_current_station_current_year = diff_dates
+                    # Check if the first measure is acquired after then current_year-01-01 00:00:00
+                    if diff_dates > 0:
+                        count_missing_data_tracts_current_station_current_year += 1
+                        len_max_missing_values_current_station_current_year = diff_dates
+                        len_min_missing_values_current_station_current_year = diff_dates
+                        len_avg_missing_values_current_station_current_year = diff_dates
                         
-                        first_day = False
+                    first_day = False
 
-                    count_total_measures_current_station_current_year += 1
+                count_total_measures_current_station_current_year += 1
 
-                    # Se siamo in presenza di un valore mancante
-                    if math.isnan(row["Concentration"]):
+                # Check if the measure is a missing value
+                if math.isnan(row["Concentration"]):
                         
-                        count_nan_measures_current_station_current_year += 1
-                        current_len_MDT += 1
+                    count_nan_measures_current_station_current_year += 1
+                    current_len_MDT += 1
 
-                        # Se è il primo valore di un tratto di missing values
-                        if current_MDT == False:
+                    # If it is the first measure of a new Missing Data Tract
+                    if current_MDT == False:
+                        count_missing_data_tracts_current_station_current_year += 1
+                        current_MDT = True
 
-                            count_missing_data_tracts_current_station_current_year += 1
-                            current_MDT = True
+                # Instead, it is a valid measure
+                else:
+                    count_valid_measures_current_station_current_year += 1
 
-                    # Se siamo in presenza di un valore numerico valido    
-                    else:
+                    # If it is the first measure after a Missing Data Tract
+                    if current_MDT == True:
 
-                        count_valid_measures_current_station_current_year += 1
+                        len_avg_missing_values_current_station_current_year += current_len_MDT
 
-                        # Se è il primo valore dopo un tratto di missing values
-                        if current_MDT == True:
+                        # Update information about the previous Missing Data Tract
+                        if len_max_missing_values_current_station_current_year < current_len_MDT:
+                            len_max_missing_values_current_station_current_year = current_len_MDT
 
-                            len_avg_missing_values_current_station_current_year += current_len_MDT
+                        if len_min_missing_values_current_station_current_year > current_len_MDT:
+                            len_min_missing_values_current_station_current_year = current_len_MDT
 
-                            # Aggiornamento se il nuovo tratto di missing values è di maggiore
-                            # lunghezza
-                            if len_max_missing_values_current_station_current_year < current_len_MDT:
-                                len_max_missing_values_current_station_current_year = current_len_MDT
+                        current_len_MDT = 0
+                        current_MDT = False
 
-                            if len_min_missing_values_current_station_current_year > current_len_MDT:
-                                len_min_missing_values_current_station_current_year = current_len_MDT
-
-                            current_len_MDT = 0
-                            current_MDT = False
-
-            # Calcolo del valore medio della lunghezza dei tratti di valori mancanti
+            # Compute the average lenght of Missing Data Tract
             if count_missing_data_tracts_current_station_current_year > 0:
                 len_avg_missing_values_current_station_current_year /= float(count_missing_data_tracts_current_station_current_year)
             
@@ -278,17 +296,17 @@ for idx_year in range(start_year,end_year):
             
             dict_country_current_year[current_station_region]["stations"][cod_station] = dict_info_current_station
 
-            # Ordinamento dizionario per lunghezza media dei valori mancanti minore
+            # Sorting the dictionary due to the minumum lenght of the maximum number of
+            # missing values of all stations
             dict_tmp = dict_country_current_year[current_station_region]["stations"].copy()
-            #list_sorted = sorted(dict_tmp.items(), key=lambda t: (t[1]["Number_of_MDTs"], t[1]["Avg_len_of_MDTs"]))
             list_sorted = sorted(dict_tmp.items(), key=lambda t: t[1]["Max_len_of_MDTs"])
 
             dict_country_current_year[current_station_region]["sorted_station_MDTs_min"] = []
             for k in range(len(list_sorted)):
                 dict_country_current_year[current_station_region]["sorted_station_MDTs_min"].append(list_sorted[k][0])
 
-    # Salvataggio del dizionario in formato JSON
-    path_current_year_json = joinpath(path_dir_air_pol_csv, str(current_year) + ".json")
+    # Save the dictionary in a JSON file
+    path_current_year_json = joinpath(path_dir_csv, str(current_year) + ".json")
     
     with open(path_current_year_json, 'w') as json_file:
         json_dumps_str = json.dumps(dict_country_current_year, indent=4)
